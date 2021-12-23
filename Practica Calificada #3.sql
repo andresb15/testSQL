30cdd05f510b2dae57eb14af98d0a9949cb3056b
andresb15 <92766327+andresb15@users.noreply.github.com>
2021-12-23 15:00:11 -0600

    Update Practica Calificada #3.sql

--- Practica Calificada #3.sql
+++ Practica Calificada #3.sql
@@ -1,11 +1,11 @@
 
-
+--test
 
 --Parte 1 Realice los siguientes procedimientos almacenados, SP_PARTE#_EJERICIO#_GRUPO#
 
 
---1. Visualizar el m·ximo y mÌnimo precio de los productos por categorÌa, mostrar el nombre de la
---categorÌa por par·metro.
+--1. Visualizar el m√°ximo y m√≠nimo precio de los productos por categor√≠a, mostrar el nombre de la
+--categor√≠a por par√°metro.
 
 
 SET ANSI_NULLS ON
@@ -16,8 +16,8 @@ GO
 -- Author:		Grupo2: Tavo, Andres Bejarano ,Luis Esquivel
 -- Create date: 23-12-2021
 -- Description:	
---1. Visualizar el m·ximo y mÌnimo precio de los productos por categorÌa, mostrar el nombre de la
---categorÌa por par·metro.
+--1. Visualizar el m√°ximo y m√≠nimo precio de los productos por categor√≠a, mostrar el nombre de la
+--categor√≠a por par√°metro.
 
 -- =============================================
 CREATE PROCEDURE SP_PARTE#1_EJERICIO#1_GRUPO#2
@@ -43,26 +43,26 @@ GO
 
 
 
---2. Visualizar el m·ximo y mÌnimo precio de los productos por proveedor, mostrar el nombre de la
---compaÒÌa proveedora por par·metro.
+--2. Visualizar el m√°ximo y m√≠nimo precio de los productos por proveedor, mostrar el nombre de la
+--compa√±√≠a proveedora por par√°metro.
 
 
 
 
 
 
-3. Seleccionar las categorÌas que tengan m·s 5 productos. Mostrar el nombre de la categorÌa y el
-n˙mero de productos por par·metro
-4. Calcular cu·ntos clientes existen en cada PaÌs
-5. Calcular cu·ntos clientes existen en cada Ciudad.
-6. Calcular cu·ntos proveedores existen en cada Ciudad y PaÌs
-7. Seleccionar cuantos proveedores tengo en cada paÌs, considerando solo al nombre de los
-proveedores que comienzan con la letra X hasta la letra x(par·metro), adem·s de mostrar
-solo los paÌsesdonde tenga m·s de 2 proveedores (par·metro).
-8. Mostrar el nombre de producto y en cuantas Ûrdenes de compra se encuentra.
+3. Seleccionar las categor√≠as que tengan m√°s 5 productos. Mostrar el nombre de la categor√≠a y el
+n√∫mero de productos por par√°metro
+4. Calcular cu√°ntos clientes existen en cada Pa√≠s
+5. Calcular cu√°ntos clientes existen en cada Ciudad.
+6. Calcular cu√°ntos proveedores existen en cada Ciudad y Pa√≠s
+7. Seleccionar cuantos proveedores tengo en cada pa√≠s, considerando solo al nombre de los
+proveedores que comienzan con la letra X hasta la letra x(par√°metro), adem√°s de mostrar
+solo los pa√≠sesdonde tenga m√°s de 2 proveedores (par√°metro).
+8. Mostrar el nombre de producto y en cuantas √≥rdenes de compra se encuentra.
 
 --Parte 2 Realizar un CRUD SP_PARTE#_EJERICIO#_GRUPO#
---ï inserciÛn de 20 registros en la tabla Clientes
---ï Actualizar 10 registros en la tabla Clientes
---ï Eliminar 5 clientes
---ï Realice un SP que contenga un buscador por Nombre de la tabla Cliente
+--‚Ä¢ inserci√≥n de 20 registros en la tabla Clientes
+--‚Ä¢ Actualizar 10 registros en la tabla Clientes
+--‚Ä¢ Eliminar 5 clientes
+--‚Ä¢ Realice un SP que contenga un buscador por Nombre de la tabla Cliente
