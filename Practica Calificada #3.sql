85eb666c5ba128272bc32eba829c1098a500e6a5
andresb15 <92766327+andresb15@users.noreply.github.com>
2021-12-25 19:10:06 -0600

    Update Practica Calificada #3.sql

--- Practica Calificada #3.sql
+++ Practica Calificada #3.sql
@@ -192,9 +192,74 @@ GO
 
 
 6. Calcular cuántos proveedores existen en cada Ciudad y País
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Grupo2: Tavo , Andres Bejarano ,Luis Esquivel
+-- Create date: 25-12-2021
+-- Description: 6. Calcular cuántos proveedores existen en cada Ciudad y País
+-- =============================================
+CREATE PROCEDURE SP_PARTE#1_EJERICIO#6_GRUPO#2 
+AS
+BEGIN
+SET NOCOUNT ON;
+SELECT [City], [Country] , COUNT([Country])
+AS 'CANTIDAD DE PROVEEDORES POR PAIS',
+COUNT([City]) AS 'CANTIDAD DE PROVEEDORES POR CIUDAD '
+FROM [dbo].[Suppliers]
+GROUP BY [City], [Country]
+END
+GO
+
+
+--7. Seleccionar cuantos proveedores tengo en cada país, considerando solo al nombre de los
+--proveedores que comienzan con la letra X hasta la letra x(parámetro), además de mostrar
+--solo los paísesdonde tenga más de 2 proveedores (parámetro).
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
 7. Seleccionar cuantos proveedores tengo en cada país, considerando solo al nombre de los
 proveedores que comienzan con la letra X hasta la letra x(parámetro), además de mostrar
 solo los paísesdonde tenga más de 2 proveedores (parámetro).
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Grupo2: Tavo , Andres Bejarano ,Luis Esquivel
+-- Create date: 26-12-2021
+-- Description:	--7. Seleccionar cuantos proveedores tengo en cada país, considerando solo al nombre de los
+--proveedores que comienzan con la letra X hasta la letra x(parámetro), además de mostrar
+--solo los paísesdonde tenga más de 2 proveedores (parámetro).
+-- =============================================
+CREATE PROCEDURE SP_PARTE#1_EJERICIO#7_GRUPO#2 
+	-- Add the parameters for the stored procedure here
+	@letra1 CHAR(40) , 
+	@letra2 CHAR(40) ,
+	@country NVARCHAR(15) 
+AS
+BEGIN
+SET NOCOUNT ON;
+SELECT [Country], COUNT(*) AS 'Cantidad de proveedores'
+FROM [dbo].[Suppliers]
+where [CompanyName]  LIKE '['+@letra1+'-'+@letra2+']%' 
+AND [Country] = @country
+GROUP BY [Country] 
+HAVING COUNT(*) > 2
+END
+GO
+
+
+
+
 8. Mostrar el nombre de producto y en cuantas órdenes de compra se encuentra.
 
 --Parte 2 Realizar un CRUD SP_PARTE#_EJERICIO#_GRUPO#
